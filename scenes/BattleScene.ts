d2b94ee2272c87b48552b7df20b1bc7b0b8ff51b
Michael Schulz <mi.schulz@hotmail.com>
2023-06-05 19:28:52 +0200

    Begin creating battle scene

---
+++ scenes/BattleScene.ts
@@ -0,0 +1,12 @@
+import Phaser from 'phaser';
+
+export default class BattleScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'BattleScene' });
+  }
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
