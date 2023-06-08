8073bb5e9824d5bc5375ede92a737623b8536fd7
Michael Schulz <mi.schulz@hotmail.com>
2023-06-08 22:59:03 +0200

    progress lab cutscene

--- scenes/LabCutscene.ts
+++ scenes/LabCutscene.ts
@@ -1,13 +1,18 @@
 import 'phaser';
 
+import { Events } from 'phaser';
+
 import DialogueField from '../dialogue/DialogueField';
 
 export default class LabCutscene extends Phaser.Scene {
+  gameEvents: Events.EventEmitter;
+  dialogueField: DialogueField;
   constructor() {
     super({ key: 'LabCutscene' });
     const dialogueField = new DialogueField();
-    const gameEvents = new Phaser.Events.EventEmitter();
-    // dialogueField.show();
+    this.gameEvents = new Phaser.Events.EventEmitter();
+    this.gameEvents.on('progressDialogue', this.progressDialogue, this);
+    dialogueField.show();
   }
 
   preload() {
@@ -16,7 +21,10 @@ export default class LabCutscene extends Phaser.Scene {
 
   create() {
     this.add.image(0, 4, 'labCutscene').setOrigin(0, 0).setScale(8);
+    this.dialogueField.show();
   }
 
   update() {}
+
+  progressDialogue() {}
 }
