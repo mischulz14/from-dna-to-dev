772f4365305856468b0463fb0ecbdea2004e85d9
Michael Schulz <mi.schulz@hotmail.com>
2023-06-11 12:14:43 +0200

    Further improve labcutscene

--- scenes/LabCutscene.ts
+++ scenes/LabCutscene.ts
@@ -3,12 +3,24 @@ import DialogueNode from '../dialogue/DialogueNode';
 
 export default class LabCutscene extends Phaser.Scene {
   dialogueController: DialogueController;
+  transitionRect: Phaser.GameObjects.Rectangle;
 
   constructor() {
     super({ key: 'LabCutscene' });
     const dialogue = [
-      new DialogueNode('You have entered the lab!'),
-      new DialogueNode('You can use the arrow keys to move around.'),
+      new DialogueNode('After a long day of work, you look out of the window.'),
+      new DialogueNode(
+        'All this battling with viruses, all of those nightshifts and sleep deprivation is certainly taking a toll on me...',
+      ),
+      new DialogueNode(
+        'This was not how I imagined it to be when I was studying to become a scientist...',
+      ),
+      new DialogueNode(
+        'Is this really what I want to do for the rest of my life?',
+      ),
+      new DialogueNode(
+        'I should probably take a vacation and maybe I will feel better afterwards...',
+      ),
     ];
     this.dialogueController = new DialogueController(this);
     this.dialogueController.initiateDialogueNodesArray(dialogue);
@@ -16,6 +28,11 @@ export default class LabCutscene extends Phaser.Scene {
 
   preload() {
     this.dialogueController.dialogueField.show();
+
+    this.load.spritesheet('LabCutsceneSprite', 'assets/labCutsceneSprite.png', {
+      frameWidth: 100,
+      frameHeight: 50,
+    });
 
     this.input.keyboard.on(
       'keydown-ENTER',
@@ -26,21 +43,68 @@ export default class LabCutscene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(0, 4, 'labCutscene').setOrigin(0, 0).setScale(8);
     this.dialogueController.dialogueField.show();
     this.dialogueController.typeText();
+
+    this.anims.create({
+      key: 'labCutsceneSprite',
+      frames: this.anims.generateFrameNumbers('LabCutsceneSprite', {
+        start: 0,
+        end: 7,
+      }),
+      frameRate: 4,
+      repeat: -1,
+    });
+
+    // Add sprite
+    const sprite = this.add.sprite(0, 0, 'LabCutsceneSprite').setOrigin(0, 0);
+
+    // Scale the sprite
+    sprite.setScale(8.2);
+
+    // Start the animation
+    sprite.play('labCutsceneSprite');
+
+    this.events.on('dialogueEnded', () => {
+      this.input.keyboard.removeAllListeners('keydown-ENTER');
+      this.dialogueController.dialogueField.hide();
+      this.cutsceneTransitionNormal();
+
+      setTimeout(() => {
+        this.scene.stop('LabCutscene');
+        this.scene.start('LabScene');
+      }, 2000);
+    });
+
+    // create transitions
+    this.transitionRect = this.add
+      .rectangle(0, 0, this.scale.width, this.scale.height, 0x000000)
+      .setOrigin(0, 0);
+    this.transitionRect.setAlpha(0); // Start with 0 opacity
   }
 
   progressDialogue() {
     this.dialogueController.progressDialogue();
-    if (
-      this.dialogueController.currentDialogueIndex >=
-      this.dialogueController.dialogue.length - 1
-    ) {
-      this.input.keyboard.removeAllListeners('keydown-ENTER');
-      this.dialogueController.dialogueField.hide();
-      this.scene.stop('LabCutscene');
-      this.scene.start('LabScene');
-    }
+  }
+
+  cutsceneTransitionNormal() {
+    this.tweens.add({
+      targets: this.transitionRect,
+      alpha: { from: 0, to: 1 },
+      ease: 'Linear',
+      duration: 1000,
+      repeat: 0,
+    });
+  }
+
+  cutsceneTransitionBlinking() {
+    this.tweens.add({
+      targets: this.transitionRect,
+      alpha: { from: 0, to: 1 },
+      ease: 'Linear',
+      duration: 300,
+      yoyo: true,
+      repeat: 3,
+    });
   }
 }
