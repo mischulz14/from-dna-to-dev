e69db887af3d39f75838014cfa5231534d35f937
Michael Schulz <mi.schulz@hotmail.com>
2023-09-20 00:37:19 +0200

    put assets into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
+
 module.exports = {
   entry: './src/app.ts',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'assets', to: 'assets' }],
     }),
   ],
   module: {
