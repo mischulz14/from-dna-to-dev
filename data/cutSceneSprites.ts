e6b3e65b1e70577c34e52470f8afb826e6ae5755
Michael Schulz <mi.schulz@hotmail.com>
2023-09-20 19:44:40 +0200

    new cutscene data

--- data/cutSceneSprites.ts
+++ data/cutSceneSprites.ts
@@ -1,8 +1,11 @@
 export const cutSceneSpriteNames = {
   wohnung: 'wohnungCutsceneIntroSprite',
+  lab: 'LabCutsceneSprite',
+  intro: 'introCutsceneSprite',
 };
 
 export const cutSceneAudioNames = {
+  intro: 'IntroScene',
   wohnung: 'WohnungCutscene',
 };
 
@@ -14,12 +17,12 @@ export const cutSceneAnimsInfo = {
       {
         name: 'layingAnScrollingAnim',
         start: 0,
-        end: 12,
+        end: 11,
         repeat: 2,
       },
       {
         name: 'phonePingAnim',
-        start: 13,
+        start: 12,
         end: 20,
       },
       {
